666d7fe3a0f3e9e06bb01c672004dd8dddd7a6a6
Gyanendra Shukla <srirajshukla@gmail.com>
2021-09-05 10:26:44 +0530

    Better Maps

--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,6 @@
-use rltk::{RGB, Rltk};
+use std::cmp::{max, min};
+
+use rltk::{RGB, RandomNumberGenerator, Rltk};
 use super::rectangle::*;
 
 #[derive(PartialEq, Copy, Clone)]
@@ -59,16 +61,60 @@ pub fn draw_map(map: &[TileType], ctx: &mut Rltk){
     }
 }
 
-pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
+pub fn new_map_rooms_and_corridors() -> (Vec<Rect>, Vec<TileType>) {
     let mut map = vec![TileType::Wall; 80*50];
     
+    let mut rooms : Vec<Rect> = Vec::new();
+    const MAX_ROOMS : i32 = 30;
+    const MIN_SIZE : i32 = 6;
+    const MAX_SIZE : i32 = 10;
+
+    let mut rng = RandomNumberGenerator::new();
+
+
+    for _ in 0..MAX_ROOMS {
+        let w = rng.range(MIN_SIZE, MAX_SIZE);
+        let h = rng.range(MIN_SIZE, MAX_SIZE);
+        let x = rng.roll_dice(1, 80-w-1) -1;
+        let y = rng.roll_dice(1, 50-h-1) -1;
+
+        let new_room = Rect::new(x,y,w,h);
+        let mut ok = true;
+        for other_room in rooms.iter(){
+            if new_room.intersect(other_room){
+                ok = false;
+            }
+        }
+
+        if ok{
+            apply_room_to_map(&mut map, &new_room);
+            
+            if !rooms.is_empty() {
+                let (new_x, new_y) = new_room.center();
+                let (prev_x, prev_y) = rooms[rooms.len()-1].center();
+
+                if rng.range(0,2)==1 {
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
+                } else{
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+                }
+            }
+
+            rooms.push(new_room);
+        }
+    }
+
     let room1 = Rect::new(20, 15, 10, 15);
     let room2 = Rect::new(35, 15, 10, 15);
 
     apply_room_to_map(&mut map, &room1);
     apply_room_to_map(&mut map, &room2);
 
-    map
+    apply_horizontal_tunnel(&mut map, 25, 40, 23);
+
+    (rooms,map)
 }
 
 
@@ -79,3 +125,22 @@ fn apply_room_to_map(map: &mut [TileType], room: &Rect) {
         }
     }
 }
+
+fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32){
+    for x in min(x1, x2) ..=max(x1,x2){
+        let idx = xy_idx(x, y);
+        if idx>0 && idx < 80*50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
+fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32){
+    for y in min(y1, y2) ..=max(y1,y2){
+        let idx = xy_idx(x, y);
+        if idx>0 && idx < 80*50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
